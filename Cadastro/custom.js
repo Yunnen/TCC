045997a662bdb60658e2b810148b53919221d3e9
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-08-06 01:35:01 -0300

    Update custom.js

--- Cadastro/custom.js
+++ Cadastro/custom.js
@@ -1,4 +1,8 @@
 
+ var btn = document.getElementById("btn_cadastro");
+
+
+
 $("#nome").blur(function() {
    
         var nome = $(this).val()
@@ -10,12 +14,17 @@ $("#nome").blur(function() {
         $('#Cnome').css('display','none');
 
         $('#nome').css('border-color','red');
+
+       
+    
     }else{
         $('#Enome').css('display', 'none');
        
         $('#Cnome').css('display','contents');
 
         $('#nome').css('border-color','green');
+
+     
     }
 });
 
@@ -36,6 +45,9 @@ $("#email").blur(function() {
         $('#Eemail').css('display', 'none');
 
         $('#email').css('border-color','green');
+
+    
+
     }else{
         
         $('#Eemail').css('display', 'contents');;
@@ -46,8 +58,9 @@ $("#email").blur(function() {
 
         document.getElementById("Eemail").innerHTML = " <i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Insira um Email Valido";
     
+      
     }
-    }
+  }
 )
 
 $("#senha").blur(function() {
@@ -62,13 +75,14 @@ if(senha.length < 8 || senha.length > 20){
 
     $('#senha').css('border-color','red');
 
-
 }else{
     $('#Esenha').css('display', 'none');
    
     $('#Csenha').css('display','contents');
 
     $('#senha').css('border-color','green');
+
+
 }
 });
 
@@ -84,15 +98,40 @@ if(senhaconfir != senha){
     $('#Csenhaconfir').css('display','none');
 
     $('#senhaconfir').css('border-color','red');
+
+   
 }else{
     $('#Esenhaconfir').css('display', 'none');
    
     $('#Csenhaconfir').css('display','contents');
 
     $('#senhaconfir').css('border-color','green');
+
+   
 }
 });
 
-function testartodos(){
-    
-}
+const Dados = document.getElementById("cadastro");
+if (Dados) {
+    Dados.addEventListener("submit", async (e) => {
+      e.preventDefault();
+  
+      const formdados = new FormData(Dados);
+      const dados = await fetch("cadastro.php", {
+        method: "POST",
+        body: formdados
+      });
+  
+      const resposta = await dados.json();
+  
+      if (resposta['status'] == true) {
+
+       console.log('Boa');
+
+       document.getElementById("Erros").innerHTML = resposta['msg'];
+      } else {
+        document.getElementById("Erros").innerHTML = resposta['msg'];
+        
+      }
+    });
+  }
