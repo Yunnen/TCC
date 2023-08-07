07eadca49af441bef130672d5486b0ae2e25d1fa
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-08-07 13:10:30 -0300

    Create login_tela.php

---
+++ Módulo_Usuário/login_tela.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="sortcut icon" href="terra-do-planeta-3185935-removebg-preview (1).png" type="image/png" />
+    <link rel="stylesheet" type="text/css" href="CSS/EstiloLoginNovo.css">
+</head>
+<body>
