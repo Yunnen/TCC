f7dc142a5147516f62d754a813befafbc3299c7b
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-10-07 18:58:09 -0300

    Update entrar.php

--- Entrar/entrar.php
+++ Entrar/entrar.php
@@ -1,14 +1,14 @@
 <?php
     
-
     $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
     include_once "../Banco_de_Dados/conexao.php";
 
-
-    if(empty($dados['email']) || !filter_var($dados['email'], FILTER_VALIDATE_EMAIL)){
+    if(empty($dados['email'])){
         $retorna = ['status' => false, 'msg' => "Insira um Email"];
-    }elseif(empty($dados['senha']) || strlen($dados['senha']) < 8 || strlen($dados['senha']) > 20){
+    }elseif(!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)){
+        $retorna = ['status' => false, 'msg' => "Insira um Email Valido"];
+    }elseif(empty($dados['senha'])){
         $retorna = ['status' => false, 'msg' => "Insira uma Senha"];
     }else{
         
@@ -21,5 +21,23 @@
             if ($search->rowCount() == 0) {
                 $retorna = ['status'=> false, 'msg'=> "Email não vinculado com uma conta, deseja fazer um cadastro?"];
             }
+            else{
+                $search = $conexão->prepare("SELECT * FROM usuarios WHERE Email_usuario = :Email AND Senha_usuario = :senha");
+    
+                $search->bindParam(':Email',$dados['email']);
+                $search->bindParam(':senha',$dados['senha']);
+                
+                $search->execute();
+
+            if ($search->rowCount() == 0) {
+                $retorna = ['status'=> false, 'msg'=> "Senha Incorreta !"];
+            }else{
+                $retorna = ['status'=> true, 'msg'=> "Bem VINDO!"];
+            }
         }
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($retorna);
 ?>
+
