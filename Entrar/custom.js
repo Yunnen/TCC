5b8508c6cd65a617c6a9c7ba803e70bddb68f881
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-10-07 18:58:05 -0300

    Update custom.js

--- Entrar/custom.js
+++ Entrar/custom.js
@@ -37,29 +37,16 @@ if (Dados) {
   
       if (resposta['status'] == true) {
 
-                 $('#loading')
-                 .css({
-                    position: "fixed", // fixa o elemento
-                    zIndex: "9", // coloca o elemento por cima dos outros
-                    top: "50%", // move 50% do topo
-                    left: "50%", // move 50% da esquerda
-                    transform: "translate(-50%, -50%)" // centraliza
-                 })
-                 .show();
-                 $('#login').css({
-                    opacity: "0.1",
-                    trasition: "1s"
-                 })
-
         setTimeout(function() {
             $(location).attr('href', '../Entrar/login.html');
         }, 5000);
        
-        console.log('Boa');
+        console.log(resposta['msg']);
 
       } else {
-       
-        document.getElementById("Erros").innerHTML = resposta['msg']; 
+        console.log(resposta['msg']);
       }
     });
+
+    
 }
