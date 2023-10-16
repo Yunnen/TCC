084c8263c5ff62a643bc5da89fe6aa30ded4d1f1
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-10-16 20:58:39 -0300

    Update custom.js

--- Cadastro/custom.js
+++ Cadastro/custom.js
@@ -1,124 +1,13 @@
+var btn = document.getElementById("btn_cadastro");
+senha = true;
+senhaconfir = true;
 
- var btn = document.getElementById("btn_cadastro");
- senha = true;
- senhaconfir = true;
+barra = 0;
 
-
-$("#nome").blur(function() {
-   
-        var nome = $(this).val()
-
-    if(nome.length < 3 || nome.length > 40){
-        document.getElementById("Enome").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Seu nome deve conter de 3 a 40 caracteres";
-        $('#Enome').css('display', 'inline');
-
-        $('#Cnome').css('display','none');
-
-        $('#nome').css('border-color','red');
-
-       
-    
-    }else{
-        $('#Enome').css('display', 'none');
-       
-        $('#Cnome').css('display','contents');
-
-        $('#nome').css('border-color','green');
-
-     
-    }
-});
-
-$("#email").blur(function() {
-     
-
-    function validateEmail(email) {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-      }
-
-      validação = validateEmail(this.value);
-      console.log(validação);
-      
-      if(validação == true){
-        $('#Cemail').css('display','contents');
-
-        $('#Eemail').css('display', 'none');
-
-        $('#email').css('border-color','green');
-
-    }else{
-        
-        $('#Eemail').css('display', 'contents');;
-        
-        $('#Cemail').css('display','none');
-
-        $('#email').css('border-color','red');
-
-        document.getElementById("Eemail").innerHTML = " <i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Insira um Email Valido";
-    
-    }
-  }
-)
-
-$("#senha").blur(function() {
-   
-    var senha = $(this).val()
-
-if(senha.length < 8 || senha.length > 20){
-    document.getElementById("Esenha").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Sua senha deve conter de 8 a 20 caracteres";
-    $('#Esenha').css('display', 'inline');
-
-    $('#Csenha').css('display','none');
-
-    $('#senha').css('border-color','red');
-
-}else{
-    $('#Esenha').css('display', 'none');
-   
-    $('#Csenha').css('display','contents');
-
-    $('#senha').css('border-color','green');
-
-
-}
-});
-
-$("#senhaconfir").blur(function() {
-   
-    var senhaconfir = $(this).val()
-    var senha = $("#senha").val();
-
-if(senhaconfir != senha){
-    document.getElementById("Esenhaconfir").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> As senhas devem coencidir";
-    $('#Esenhaconfir').css('display', 'inline');
-
-    $('#Csenhaconfir').css('display','none');
-
-    $('#senhaconfir').css('border-color','red');
-
-   
-}else if(senhaconfir == ""){
-    document.getElementById("Esenhaconfir").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Confirme sua senha";
-    $('#Esenhaconfir').css('display', 'inline');
-
-    $('#Csenhaconfir').css('display','none');
-
-    $('#senhaconfir').css('border-color','red');
-
-}
-
-
-else{
-    $('#Esenhaconfir').css('display', 'none');
-   
-    $('#Csenhaconfir').css('display','contents');
-
-    $('#senhaconfir').css('border-color','green');
-
-   
-}
-});
+var nomee = 0;
+var email = 0;
+var senhaa = 0;
+var senhac = 0;
 
 const Dados = document.getElementById("cadastro");
 
@@ -127,6 +16,7 @@ if (Dados) {
       e.preventDefault();
   
       const formdados = new FormData(Dados);
+
       const dados = await fetch("cadastro.php", {
         method: "POST",
         body: formdados
@@ -158,54 +48,265 @@ if (Dados) {
 
       } else {
        
-        document.getElementById("Erros").innerHTML = resposta['msg']; 
+        console.log(resposta['msg']);
+
+      
       }
     });
 
     function MostrarSenha(){
         
         var btn = document.getElementById("senha");
+
+        var image = document.getElementById("olho");
         
         if(senha == true){
         
         btn.type = "text";
         this.senha = false;
 
-        $("#icon_senha").removeClass("fa-solid fa-eye").addClass("fa-solid fa-eye-slash");
+        image.src = "../IMG/eye-off 16.svg";
+
 
     }else{
+
         
         btn.type = "password"
         this.senha = true;
 
-        $("#icon_senha").removeClass("fa-solid fa-eye-slash").addClass("fa-solid fa-eye");
+        image.src = "../IMG/eye.svg";
+
 
     }
     }
     function MostrarConfirme(){
-        
-        
-        var btn = document.getElementById("senhaconfir");
-        var icon = document.getElementById("icon_senhaconfirm");
+        var btn = document.getElementById("senhaC");
+
+        var image = document.getElementById("olhoC");
         
         if(senhaconfir == true){
         
         btn.type = "text";
         this.senhaconfir = false;
 
-        
-        btn.innerHTML = "";
-        
-        $("#icon_senhaconfirm").removeClass("fa-solid fa-eye").addClass("fa-solid fa-eye-slash");
+        image.src = "../IMG/eye-off 16.svg";
+
 
     }else{
-        
+
         btn.type = "password"
         this.senhaconfir = true;
 
-        btn.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
+        image.src = "../IMG/eye.svg";
 
-        $("#icon_senhaconfirm").removeClass("fa-solid fa-eye-slash").addClass("fa-solid fa-eye");
     }
-    }
+}
+}
+
+
+$("#nome").blur(function() {
+   
+  var nome = $(this).val()
+
+if(nome.length < 3 || nome.length > 40){
+
+
+  document.getElementById("erronome").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Seu nome deve conter de 3 a 40 caracteres";
+
+  $('#nome').css('border-color','red');
+
+
+  if(nomee == 0){
+    console.log(nomee);
+  }else{
+    nomee = 0;
+    barra--;
   }
+
+}else{
+
+  document.getElementById("erronome").innerHTML = null;
+
+  $('#nome').css('border-color','#d14919');
+
+  if(nomee == 1){
+    console.log(nomee);
+  }else{
+    nomee = 1;
+    barra++;
+  }
+}
+console.log(barra);
+carregarbarra();
+});
+
+$("#email").blur(function() {
+
+
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+validação = validateEmail(this.value);
+console.log(validação);
+
+if(validação == true){
+ 
+  $('#email').css('border-color','#d14919');
+
+  document.getElementById("erroemail").innerHTML = null;
+
+ 
+  if(email == 1){
+    console.log(email);
+  }else{
+    email = 1;
+    barra++;
+  }
+
+}else{
+  
+  $('#email').css('border-color','red');
+
+  document.getElementById("erroemail").innerHTML = " <i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Insira um Email Valido";
+
+ 
+  if(email == 0){
+    console.log(nomee);
+  }else{
+    email = 0;
+    barra--;
+  }
+
+}
+console.log(barra);
+carregarbarra();
+}
+)
+
+$("#senha").blur(function() {
+
+var senha = $(this).val()
+
+if(senha.length < 8 || senha.length > 20){
+
+document.getElementById("errosenha").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Sua senha deve conter de 8 a 20 caracteres";
+
+$('#senha').css('border-color','red');
+
+if(senhaa == 0){
+  console.log(senhaa);
+}else{
+  senhaa = 0;
+  barra--;
+}
+
+}else{
+
+$('#senha').css('border-color','#d14919');
+
+document.getElementById("errosenha").innerHTML = null;
+
+if(senhaa == 1){
+  console.log(senhaa);
+}else{
+  senhaa = 1;
+  barra++;
+}
+
+}
+carregarbarra();
+});
+
+$("#senhaC").blur(function() {
+
+var senhaconfir = $(this).val()
+var senha = $("#senha").val();
+
+if(senhaconfir != senha){
+document.getElementById("errosenha2").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> As senhas devem coencidir";
+
+
+$('#senhaC').css('border-color','red');
+
+if(senhac == 0){
+  console.log(senhaC);
+}else{
+  senhac = 0;
+  barra--;
+}
+
+
+}else if(senhaconfir == ""){
+
+document.getElementById("errosenha2").innerHTML = "<i class='fa-regular fa-circle-xmark' style='color: #b21f1f;'></i> Confirme sua senha";
+
+
+if(senhac == 0){
+  console.log(senhaC);
+}else{
+  senhac = 0;
+  barra--;
+}
+
+$('#senhaC').css('border-color','red');
+
+}
+
+else{
+
+$('#senhaC').css('border-color','#d14919');
+
+document.getElementById("errosenha2").innerHTML = null;
+
+
+if(senhac == 1){
+  console.log(senhac);
+}else{
+  senhac = 1;
+  barra++;
+}
+}
+carregarbarra();
+});
+
+
+function carregarbarra(){
+
+    switch (barra){
+      case 0:
+        $('#1').css('background-color','#1d1d1d');
+        $('#2').css('background-color','#1d1d1d');
+        $('#3').css('background-color','#1d1d1d');
+        $('#4').css('background-color','#1d1d1d');
+        break;
+      case 1:
+        $('#1').css('background-color','#d14919');
+        $('#2').css('background-color','#1d1d1d');
+        $('#3').css('background-color','#1d1d1d');
+        $('#4').css('background-color','#1d1d1d');
+        break;
+      case 2:
+        $('#1').css('background-color','#d14919');
+        $('#2').css('background-color','#d14919');
+        $('#3').css('background-color','#1d1d1d');
+        $('#4').css('background-color','#1d1d1d');
+        break;
+      case 3:
+        $('#1').css('background-color','#d14919');
+        $('#2').css('background-color','#d14919');
+        $('#3').css('background-color','#d14919');
+        $('#4').css('background-color','#1d1d1d');
+        break;
+      case 4:
+        $('#1').css('background-color','#d14919');
+        $('#2').css('background-color','#d14919');
+        $('#3').css('background-color','#d14919');
+        $('#4').css('background-color','#d14919');
+        break;
+
+    }
+}
+
+
