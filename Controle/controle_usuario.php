e58a6b102ead228d9ed24603de7562158041ed52
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-10-24 23:36:48 -0300

    Create controle_usuario.php

---
+++ Controle/controle_usuario.php
@@ -0,0 +1,13 @@
+<?php
+
+
+  session_start();
+//Puxar a Img do Usuario e colocar dentro da viaravel $Foto
+if(isset($_SESSION['Foto_Usuario'])){
+    $foto = $_SESSION['Foto_Usuario'];
+  }else{
+    $foto = "IMG/Pyra.png";
+  }
+
+
+?>
