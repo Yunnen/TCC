fc2265cb554249d61520ef3f0622ca0e93c9e723
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-10-24 23:36:52 -0300

    Update custom.js

--- Entrar/custom.js
+++ Entrar/custom.js
@@ -27,8 +27,16 @@ function MostrarSenha(){
     }
 
     const Dados = document.getElementById("entrar");
-if (Dados) {
-    Dados.addEventListener("submit", async (e) => {
+    const botão = document.getElementById("btn_login");
+    const zoio = document.getElementById("zoio");
+if (zoio){
+    zoio.addEventListener("click", async (e) => {
+      e.preventDefault();})
+ } 
+
+if (botão) {
+
+    botão.addEventListener("click", async (e) => {
       e.preventDefault();
   
       const formdados = new FormData(Dados);
@@ -41,23 +49,31 @@ if (Dados) {
   
       if (resposta['status'] == true) {
 
-        setTimeout(function() {
-            $(location).attr('href', '../Entrar/login.html');
-        }, 5000);
+
+            $(location).attr('href', '../index.php');
+      
        
         console.log(resposta['msg']);
 
         document.getElementById("msg").innerHTML = null;
 
       }else if(resposta['problema'] == 'email') {
+
         document.getElementById("msgemail").innerHTML = resposta['msg'];
 
-        document.getElementById("msgesenha").innerHTML = null;
+        
+
+        $('#email').css('border-color','red');
+
       }else{
+
+        $('#email').css('border-color','#ff5b1b');
 
         document.getElementById("msgemail").innerHTML = null;
 
         document.getElementById("msgsenha").innerHTML = resposta['msg'];
+
+        $('#senha').css('border-color','red');
       }
     });
 
