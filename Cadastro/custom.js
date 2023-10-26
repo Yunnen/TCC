4e4f6bf421cd4221886b1f0e9e46ccd7a935c433
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-10-26 20:14:14 -0300

    Update custom.js

--- Cadastro/custom.js
+++ Cadastro/custom.js
@@ -31,6 +31,20 @@ if (Dados) {
       const resposta = await dados.json();
   
       if (resposta['status'] == true) {
+
+        $('#loading')
+        .css({
+           position: "fixed", // fixa o elemento
+           zIndex: "9", // coloca o elemento por cima dos outros
+           top: "50%", // move 50% do topo
+           left: "50%", // move 50% da esquerda
+           transform: "translate(-50%, -50%)" // centraliza
+        })
+        .show();
+        $('#cadastro').css({
+           opacity: "0.1",
+           trasition: "1s"
+        })
         setTimeout(function() {
             $(location).attr('href', '../Entrar/login.html');
         }, 10000);
